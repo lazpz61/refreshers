93d3f53ffe395e375c1ffb6a83ec7b6ab86fee47
LazaroMPerez <lazpz61@gmail.com>
2021-05-23 22:02:54 -0400

    refresehd foreach

---
+++ refresehers.js
@@ -0,0 +1,23 @@
+toUC = (word) => {
+    let upper = word.toUpperCase();
+    let arr = upper.concat(" ", "throw")
+    return arr.split(" ");
+  }
+  
+  toUC("basketball");
+  
+  
+
+
+  const arr = ["laz", "ayisha", "rabia"]
+  
+  
+  arr.forEach( element =>{
+    let bigWord = element.toUpperCase();
+    console.log(bigWord);
+  })
+
+
+  
+
+  
